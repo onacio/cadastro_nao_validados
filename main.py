84f938df07d68ca49f66b1f3547578d476365e25
Onácio Santana <nsonacio@hotmail.com>
2025-09-07 08:58:33 -0300

    atualização

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import pandas as pd
 
 
-# planilha_esus = pd.read_csv('esus.csv', sep=';', encoding='latin1', skiprows=17)
-# planilha_sisab = pd.read_csv('sisab.csv', sep=';', encoding='latin1', skiprows=10)
-
 # Lê os arquivos CSV
 df1 = pd.read_csv('esus.csv', sep=';', encoding='latin1', skiprows=17)
 df2 = pd.read_csv('sisab.csv', sep=';', encoding='latin1', skiprows=10)
@@ -16,7 +13,7 @@ nomes2 = df2['Nome']
 df2['presente_no_segundo'] = nomes2.isin(nomes1)
 
 # Mostra os resultados
-print(df2['Nome'])
+print(nomes1)
 
 # Salva os resultados se desejar
 df2.to_csv('comparacao_resultado.csv', index=False)
